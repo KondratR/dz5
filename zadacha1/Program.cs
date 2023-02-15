1d1afb20139be506209c43ac8c521e6cfef3ebcf
Kondrat <yo126@yandex.ru>
2023-02-15 21:48:06 +0300

    zadacha1

--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] GenArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+
+int GetResult(int[] myArray)
+{
+    int result = 0;
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i] % 2 == 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+
+int[] myArray = GenArray(10, 100, 999);
+PrintArray(myArray);
+System.Console.WriteLine($"Количество четных чисел = {GetResult(myArray)}");
